398e35d8e9ebdf27d38ae80d48fab42fc15b457c
izatut <izatu1202@gmail.com>
2025-04-13 17:28:53 +0200

    Initial commit

--- app.py
+++ app.py
@@ -4,5 +4,36 @@
 # In[ ]:
 
 
-get_ipython().run_cell_magic('file', 'app.py', 'from flask import Flask, jsonify, request\n\napp = Flask(__name__)\n\n\n@app.route(\'/api/v1.0/predict\')\ndef home3():\n    # Funkcja do sprawdzenia, czy wartość jest liczbą zmiennoprzecinkową\n    def get_float_or_zero(value):\n        try:\n            return float(value) \n        except ValueError:\n            return 0.0\n\n    num1 = request.args.get("num1", 0)\n    num2 = request.args.get("num2", 0)\n\n    num1 = get_float_or_zero(num1)\n    num2 = get_float_or_zero(num2)\n\n    result = 1 if (num1 + num2) > 5.8 else 0\n\n    return jsonify({\n        "prediction": result,\n        "features": {\n            "num1": num1,\n            "num2": num2\n        }\n    })\n\nif __name__ == \'__main__\':\n    app.run()\n')
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1.0/predict')
+def home3():
+    # Funkcja do sprawdzenia, czy wartość jest liczbą zmiennoprzecinkową
+    def get_float_or_zero(value):
+        try:
+            return float(value) 
+        except ValueError:
+            return 0.0
+
+    num1 = request.args.get("num1", 0)
+    num2 = request.args.get("num2", 0)
+
+    num1 = get_float_or_zero(num1)
+    num2 = get_float_or_zero(num2)
+
+    result = 1 if (num1 + num2) > 5.8 else 0
+
+    return jsonify({
+        "prediction": result,
+        "features": {
+            "num1": num1,
+            "num2": num2
+        }
+    })
+
+if __name__ == '__main__':
+    app.run()
 
